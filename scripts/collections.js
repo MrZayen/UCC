8658544fcd732680c9b731759eedd963d7eee631
MrZayen <shubhajit3012@gmail.com>
2024-09-21 14:22:08 +0530

    update scripts/collections.js

--- scripts/collections.js
+++ scripts/collections.js
@@ -12,6 +12,19 @@ const collections = [
       getInformation: {getTitle: "Remaining Amount", getAmount: "₹3500"}
     }],
     getAmount: "₹3500"
+  },
+  {
+    getId: "07092024",
+    getLogo: "S",
+    getLogoInfo: {getType: "text", getColor: colors[3]},
+    getInformation: {getDay: "Saturday", getDate: "7 Sep, 2024"},
+    getCollections: [{
+      getClasses: ["#","#","remove"],
+      getLogo: "./images/ic_default.jpg",
+      getLogoInfo: {getType: "image"},
+      getInformation: {getTitle: "Water Tank", getAmount: "₹5000"}
+    }],
+    getAmount: "₹5000"
   }
   
   ];
