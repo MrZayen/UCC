577a6d8296bc484142674f8a2c20d4e997c5a386
MrZayen <shubhajit3012@gmail.com>
2024-09-21 13:57:07 +0530

    update scripts/collections.js

--- scripts/collections.js
+++ scripts/collections.js
@@ -1,7 +1,19 @@
 const colors = ["#FCC834","#AF5CF6", "#ED675C", "#4DCDE6", "#FF63B7", "#FA8F3D", "#5CB973", "#ED675C"];
 const collections = [
-
- 
+  {
+    id: 05092024,
+    getLogo: "T",
+    getLogoInfo: {getType: "text", getColor: colors[5]},
+    getInformation: {getDay: "Thursday", getDate: "5 Sep, 2024"},
+    getCollections: [{
+      getClasses: ["#","#","remove"],
+      getLogo: "./images/ic_default.jpg",
+      getLigoInfo: {getType: "image"},
+      getInformation: {getTitle: "Remaining Amount", getAmount: "₹3500"}
+    }],
+    getAmount: "₹3500"
+  }
+  
   ];
 
 export default collections;
