15f4b5371704ebaa9e02e743e49d7cd9cda2af9e
MrZayen <shubhajit3012@gmail.com>
2024-09-21 09:44:30 +0530

    update scripts/collections.js

--- scripts/collections.js
+++ scripts/collections.js
@@ -1,11 +1,17 @@
 const colors = ["#FCC834","#AF5CF6", "#ED675C", "#4DCDE6", "#FF63B7", "#FA8F3D", "#5CB973", "#ED675C"];
 const collections = [
   {
-    id: 12345678,
-    collections: [{},{},{},{}]
-  },{
-    id: 12345678,
-    collections: [{},{},{},{}]
+    getId: 20092024,
+    getLogo: "./android.png",
+    getLogoInfo: {getType: "image"},
+    getInformation: {getDay: "Sunday", getDate: "20 Sep, 2024"},
+    getCollections: [{
+                       getClasses: ["#","#","remove"],
+                       getLogo: "./android.png",
+                       getLogoInfo: {getType: "image"},
+                       getInformation: {getTitle: "Road Collections", getAmount: 500}
+                     }],
+    getAmount: 700
   }];
 
 export default collections;
