2205a1be7f7fa414739d0869d6b8e4a4568e287e
MrZayen <shubhajit3012@gmail.com>
2024-09-21 12:48:00 +0530

    update scripts/collections.js

--- scripts/collections.js
+++ scripts/collections.js
@@ -2,14 +2,38 @@ const colors = ["#FCC834","#AF5CF6", "#ED675C", "#4DCDE6", "#FF63B7", "#FA8F3D",
 const collections = [
   {
     getId: 20092024,
-    getLogo: "./android.png",
-    getLogoInfo: {getType: "image"},
+    getLogo: "S",
+    getLogoInfo: {getType: "text", getColor: colors[1]},
     getInformation: {getDay: "Sunday", getDate: "20 Sep, 2024"},
     getCollections: [{
-                       getClasses: ["#","#","remove"],
-                       getLogo: "./android.png",
+                       getClasses: ["#","#","#"],
+                       getLogo: "./images/ic_default.jpg",
                        getLogoInfo: {getType: "image"},
-                       getInformation: {getTitle: "Road Collections", getAmount: 500}
+                       getInformation: {getTitle: "Road Collections", getAmount: "₹500"}
+                     },{
+                       getClasses: ["#","#","remove"],
+                       getLogo: "./images/ic_default.jpg",
+                       getLogoInfo: {getType: "image"},
+                       getInformation: {getTitle: "Road Collections", getAmount: "₹500"}
+                     }],
+    getAmount: 700
+  },
+  
+  {
+    getId: 20092024,
+    getLogo: "S",
+    getLogoInfo: {getType: "text", getColor: colors[1]},
+    getInformation: {getDay: "Sunday", getDate: "20 Sep, 2024"},
+    getCollections: [{
+                       getClasses: ["#","#","#"],
+                       getLogo: "./images/ic_default.jpg",
+                       getLogoInfo: {getType: "image"},
+                       getInformation: {getTitle: "Road Collections", getAmount: "₹500"}
+                     },{
+                       getClasses: ["#","#","remove"],
+                       getLogo: "./images/ic_default.jpg",
+                       getLogoInfo: {getType: "image"},
+                       getInformation: {getTitle: "Road Collections", getAmount: "₹500"}
                      }],
     getAmount: 700
   },
@@ -20,17 +44,39 @@ const collections = [
     getInformation: {getDay: "Sunday", getDate: "20 Sep, 2024"},
     getCollections: [{
                        getClasses: ["#","#","#"],
-                       getLogo: "./android.png",
+                       getLogo: "./images/ic_default.jpg",
                        getLogoInfo: {getType: "image"},
-                       getInformation: {getTitle: "Road Collections", getAmount: 500}
+                       getInformation: {getTitle: "Road Collections", getAmount: "₹500"}
                      },{
                        getClasses: ["#","#","remove"],
-                       getLogo: "./android.png",
+                       getLogo: "./images/ic_default.jpg",
                        getLogoInfo: {getType: "image"},
-                       getInformation: {getTitle: "Road Collections", getAmount: 500}
+                       getInformation: {getTitle: "Road Collections", getAmount: "₹500"}
+                     }],
+    getAmount: 700
+  },
+  
+  {
+    getId: 20092024,
+    getLogo: "S",
+    getLogoInfo: {getType: "text", getColor: colors[1]},
+    getInformation: {getDay: "Sunday", getDate: "20 Sep, 2024"},
+    getCollections: [{
+                       getClasses: ["#","#","#"],
+                       getLogo: "./images/ic_default.jpg",
+                       getLogoInfo: {getType: "image"},
+                       getInformation: {getTitle: "Road Collections", getAmount: "₹500"}
+                     },{
+                       getClasses: ["#","#","remove"],
+                       getLogo: "./images/ic_default.jpg",
+                       getLogoInfo: {getType: "image"},
+                       getInformation: {getTitle: "Road Collections", getAmount: "₹500"}
                      }],
     getAmount: 700
   }
+ 
+ 
+ 
   ];
 
 export default collections;
