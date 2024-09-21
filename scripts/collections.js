9704a554ffd7a4be1bcdff5d71e26033c5bf1f92
MrZayen <shubhajit3012@gmail.com>
2024-09-21 15:42:07 +0530

    update scripts/collections.js

--- scripts/collections.js
+++ scripts/collections.js
@@ -25,6 +25,19 @@ const collections = [
       getInformation: {getTitle: "Water Tank", getAmount: "₹5000"}
     }],
     getAmount: "₹5000"
+  },
+  {
+    getId: "12092024",
+    getLogo: "T",
+    getLogoInfo: {getType: "text", getColor: colors[1]},
+    getInformation: {getDay: "Thursday", getDate: "12 Sep, 2024"},
+    getCollections: [{
+      getClasses: ["#","#","remove"],
+      getLogo: "./images/ic_default.jpg",
+      getLogoInfo: {getType: "image"},
+      getInformation: {getTitle: "Gorur Collection", getAmount: "₹50"}
+    }],
+    getAmount: "₹50"
   }
   
   ];
