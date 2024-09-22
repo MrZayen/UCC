ffc3d6e5efa46f94041001a47d9a96980b11d2c7
MrZayen <shubhajit3012@gmail.com>
2024-09-22 20:55:33 +0530

    update scripts/expenses.js

--- scripts/expenses.js
+++ scripts/expenses.js
@@ -1,4 +1,21 @@
 const colors = ["#FCC834","#AF5CF6", "#ED675C", "#4DCDE6", "#FF63B7", "#FA8F3D", "#5CB973", "#ED675C"];
-const expenses = [];
-
+const images = {
+  icDefault: "./images/ic_default.jpg",
+  icCow: "./images/ic_cow.jpg",
+  icVillage: "./images/ic_village.jpeg"
+};
+const expenses = [
+  {
+    getId: "02092024",
+    getLogo: "M",
+    getLogoInfo: {getType: "text", getColor: colors[3]},
+    getInformation: {getDay: "Monday", getDate: "2 Sep, 2024"},
+    getExpenses: [{
+      getClasses: ["#","#","remove"],
+      getLogo: images.icDefault,
+      getLogoInfo: {getType: "image"},
+      getInformation: {getTitle: "", getAmount: "₹500"}
+    }],
+    getAmount: "₹600"
+  }];
 export default expenses;
