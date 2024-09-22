27f0a146412c0d174d24b10f3ef1d52cc2dfa0f7
MrZayen <shubhajit3012@gmail.com>
2024-09-22 19:33:33 +0530

    update scripts/collections.js

--- scripts/collections.js
+++ scripts/collections.js
@@ -1,4 +1,8 @@
 const colors = ["#FCC834","#AF5CF6", "#ED675C", "#4DCDE6", "#FF63B7", "#FA8F3D", "#5CB973", "#ED675C"];
+const images = {
+  icCow: "./images/ic_cow.jpg",
+  icVillage: "./images/ic_village.jpeg"
+};
 const collections = [
   {
     getId: "05092024",
@@ -52,7 +56,7 @@ const collections = [
     },
     {
       getClasses: ["#","#","remove"],
-      getLogo: "./images/ic_default.jpg",
+      getLogo: images.icVillage,
       getLogoInfo: {getType: "image"},
       getInformation: {getTitle: "Village Collection", getAmount: "₹300"}
     }],
