a7c925ea1af2f252463522f9dce5245fe9d54fb0
MrZayen <shubhajit3012@gmail.com>
2024-09-29 10:28:14 +0530

    update scripts/expenses.js

--- scripts/expenses.js
+++ scripts/expenses.js
@@ -42,7 +42,7 @@ const expenses = [{
   getInformation: {getDay: getDay[1], getDate: "2 Sep, 2024"},
   getExpenses: [{
     getClasses: [getBtnClasses.system, getBtnClasses.systemIsShow, "#"],
-    getImage: getImage.system,
+    getImage: getImages.system,
     getLogoInfo: {getType: "img", isPng: false},
     getInformation: {getTitle: "পূজার রশিদ বায়না", getAmount: 500}
   },{
@@ -61,7 +61,7 @@ const expenses = [{
   getInformation: {getDay: getDay[4], getDate: "5 Sep, 2024"},
   getExpenses: [{
     getClasses: [getBtnClasses.system, getBtnClasses.systemIsShow, "#"],
-    getImage: getImage.system,
+    getImage: getImages.system,
     getLogoInfo: {getType: "img", isPng: false},
     getInformation: {getTitle: "রশিদের অবশিষ্ট টাকা", getAmount: 3970}
   },{
